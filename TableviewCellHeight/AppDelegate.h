7bb88ccbd62782f75447c1614bf490edb67e40d2
GaoYu <hhgz9527@gmail.com>
2015-04-03 16:00:25 +0800

    Initial Commit

---
+++ TableviewCellHeight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableviewCellHeight
+//
+//  Created by Yu Gao on 4/3/15.
+//  Copyright (c) 2015 thoughtworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableviewCellHeight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableviewCellHeight
+//
+//  Created by Yu Gao on 4/3/15.
+//  Copyright (c) 2015 thoughtworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
